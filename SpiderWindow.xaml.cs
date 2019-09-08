dfa1f278ea6af7186a6d17945505e8618190c48a
René Larch <renelarch@gmail.com>
2019-09-08 13:14:42 +0200

    renaming

--- SpiderWindow.xaml.cs
+++ SpiderWindow.xaml.cs
@@ -7,27 +7,27 @@ using IWin32Window = System.Windows.Forms.IWin32Window;
 
 namespace LarchSys.Bot {
     public partial class SpiderWindow : Window, IWin32Window {
-        public Spider Context { get; set; }
+        public Spider Spider { get; set; }
 
-        public SpiderWindow(Spider context)
+        public SpiderWindow(Spider spider)
         {
             InitializeComponent();
 
-            context.Window = this;
+            spider.Window = this;
 
-            DataContext = Context = context;
-            Title = Context.Title;
-            LogoPanel.Children.Add(Context.Logo);
+            DataContext = Spider = spider;
+            Title = Spider.Title;
+            LogoPanel.Children.Add(Spider.Logo);
 
-            BtnSearch.Click += async (s, e) => await Context.Search();
-            BtnExport.Click += async (s, e) => await Context.Export();
-            BtnReset.Click += async (s, e) => await Context.Reset();
+            BtnSearch.Click += async (s, e) => await Spider.Search();
+            BtnExport.Click += async (s, e) => await Spider.Export();
+            BtnReset.Click += async (s, e) => await Spider.Reset();
 
             TxbSearch.KeyDown += async (s, e) => {
-                if (e.Key == Key.Enter) await Context.Search();
+                if (e.Key == Key.Enter) await Spider.Search();
             };
 
-            Context.Clear();
+            Spider.Clear();
         }
 
         public IntPtr Handle => new WindowInteropHelper(this).Owner;
